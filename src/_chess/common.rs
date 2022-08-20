e53d070f65acf3eb514b949572d55d4916f7a7d1
Valentin Colin <valentin.colin78@gmail.com>
2022-08-20 02:09:17 +0200

    remove old files

--- src/_chess/common.rs
+++
@@ -1,6 +0,0 @@
-//! Common module
-
-pub(crate) struct Position {
-    x: i16,
-    y: i16,
-}
--- src/_chess/piece.rs
+++
@@ -1,9 +0,0 @@
-//! Describe all pieces and their interactions
-
-use super::board::Board;
-use super::common::Position;
-
-pub(crate) trait Piece {
-    fn get_side(&self) -> ();
-    fn is_valid_move(&self, from: Position, to: Position, board: Board) -> bool;
-}
